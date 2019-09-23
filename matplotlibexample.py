7d4b2a7e44fd2d33c2a9a4802329be6fee8f9ed3
yangming <yang756260386@gmail.com>
2019-09-23 10:41:55 +0800

    allok

--- matplotlibexample.py
+++ matplotlibexample.py
@@ -8,10 +8,10 @@ x = np.arange(0., 10, 0.2)
 y1 = np.cos(x)
 y2 = np.sin(x)
 y3 = np.sqrt(x)
-制 3 条函数曲线
+#制 3 条函数曲线
 plt.plot(x, y1, color='blue', linewidth=1.5, linestyle='-', marker='.', label=r'$y = cos{x}$')
 
 plt.plot(x, y2, color='green', linewidth=1.5, linestyle='-', marker='*', label=r'$y = sin{x}$')
 
 plt.plot(x, y3, color='m', linewidth=1.5, linestyle='-', marker='x', label=r'$y = \sqrt{x}$')
-
+plt.show()
